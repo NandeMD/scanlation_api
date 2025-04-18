5103d5442184369b7e00d3ba1c11c0501cce1952
Umut Can Çelik <celikumutcan2001@gmail.com>
2025-04-18 11:57:44 +0300

    added CORS middleware

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.dependencies.auth import CheckTokenDep
 from app.dependencies.helpers.auth import create_database
 from app.routers import main_router
+
+CORS = ["*"]
 
 
 @asynccontextmanager
@@ -18,7 +21,15 @@ async def lifespan(app: FastAPI):
     # Code to run at shutdown
     print("Shutting down...")
 
+
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=CORS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(main_router)
 
 
@@ -30,4 +41,3 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(_: CheckTokenDep, item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
-
